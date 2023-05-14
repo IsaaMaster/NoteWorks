dd322652ae309ab86eda4ea7b9f0f19154750667
IsaaMaster <songisaac17@gmail.com>
2023-05-14 15:31:08 -0700

    database update

--- login/settings.py
+++ login/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'login.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'VNxUAmA2oIOcRWve7N0r',
+        'HOST': 'containers-us-west-92.railway.app',
+        'PORT': '5722',
     }
 }
 
