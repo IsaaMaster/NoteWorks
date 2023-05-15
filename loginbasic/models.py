df7ac3ec6ee90c6f0836d10b5f62ece35983cfce
IsaaMaster <songisaac17@gmail.com>
2023-05-14 21:40:40 -0700

    cummalative updates

--- loginbasic/models.py
+++ loginbasic/models.py
@@ -17,6 +17,8 @@ class Folder(models.Model):
     title = models.CharField(max_length=50)
     folder = models.IntegerField(default=0)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
 
 
 
