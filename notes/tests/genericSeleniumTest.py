7efd1f2a470c49c1a9576d78ac1718f663e9ae89
IsaaMaster <songisaac17@gmail.com>
2024-10-10 19:50:37 -0700

    added headless option

--- notes/tests/genericSeleniumTest.py
+++ notes/tests/genericSeleniumTest.py
@@ -18,7 +18,7 @@ class BasicTest(unittest.TestCase):
         we have this to avoid dev tools logs that are not related to tests.
         """
         options = Options()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-gpu")
         options.add_argument("--log-level=3")
