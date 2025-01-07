629049ef164cfe5a65702dd443f853fae03ad919
IsaaMaster <songisaac17@gmail.com>
2025-01-07 13:10:00 -0800

    update test URL for Selenium authentication tests

--- notes/tests/test_selenium_authentication.py
+++ notes/tests/test_selenium_authentication.py
@@ -9,7 +9,7 @@ import unittest
 class TestAuth(BasicTest):
     def test_home_page(self):
         print('testing home page')
-        self.driver.get('https://localhost:8000')
+        self.driver.get('https://noteworks5-0c3e8b44eefc.herokuapp.com/')
         self.assertIn('NoteWorks', self.driver.title)
 
     def test_login_page(self):
